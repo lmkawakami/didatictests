3a078ff322b865fea0f14d57b721ab928fa77b46
lmkawakami <lmkawakami@hotmail.com>
2022-01-09 09:31:36 -0300

    starting over using another template

---
+++ didatictests/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for didatictests."""
+
+__author__ = "Lincoln Makoto Kawakami"
+__email__ = "lmkawakami@hotmail.com"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.0.2"
+
+
+def get_module_version():
+    return __version__
+
+
+from .example import Example  # noqa: F401
